48ced333c29ae0876b8a752708cfa3d7913eb887
SamyukthaAnanth <79256266+SamyukthaAnanth@users.noreply.github.com>
2021-07-21 18:52:23 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -1,7 +1,7 @@
 public class sum {
     public static void main(String args[]) {
       int x=10;
-      int y=25;
+      int y=20;
       int z=x+y;
 
       System.out.println("Sum of x+y = " + z);
